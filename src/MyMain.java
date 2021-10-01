b7432220f10ef70ad46f74b166289ac9f4266e77
Alexander Hofmann <alexander.hofmann@technikum-wien.at>
2021-10-01 12:43:51 +0200

    Added new line to print...

--- src/MyMain.java
+++ src/MyMain.java
@@ -5,6 +5,7 @@ public class MyMain {
     public static void main(String[] args) {
         System.out.println("Shapes and Figures V0.0.1");
         System.out.println("(C) 2021 by Alexander Hofmann");
+        System.out.println("TEST TEST TEST");
     }
 
 
